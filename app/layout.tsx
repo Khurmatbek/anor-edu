ecc9e4e4a8933666e9582b6c45c5ecfd8f236d9b
Khurmatbek <your_email@example.com>
2025-07-01 22:13:14 +0500

    test

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from 'next'
 import './globals.css'
-import Head from 'next/head'
 
 
 
@@ -30,9 +29,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
-        <meta name="theme-color" content="#0f172a" />
-      </Head>
       <body>
         {children}
       </body>
