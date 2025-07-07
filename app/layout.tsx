cf96ca0d2ddc9298a8810fa57902dc590baf6a8c
Khurmatbek <your_email@example.com>
2025-07-07 15:07:17 +0500

    test metrika

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import './globals.css'
-import Script from 'next/script' // GTM uchun kerak
+import Script from 'next/script'
 
 export const metadata: Metadata = {
   title: 'Anor International School',
@@ -29,7 +29,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* Google Tag Manager script (faqat productionda) */}
+        {/* Google Tag Manager (faqat production) */}
         {process.env.NODE_ENV === "production" && (
           <Script id="gtm-script" strategy="afterInteractive">
             {`
@@ -42,10 +42,34 @@ export default function RootLayout({
           </Script>
         )}
 
+        {/* Yandex Metrika (faqat production) */}
+        {process.env.NODE_ENV === "production" && (
+          <Script id="yandex-metrika" strategy="afterInteractive">
+            {`
+              (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){
+                  (m[i].a=m[i].a||[]).push(arguments)};
+                  m[i].l=1*new Date();
+                  for (var j = 0; j < document.scripts.length; j++) {
+                    if (document.scripts[j].src === r) { return; }
+                  }
+                  k=e.createElement(t),a=e.getElementsByTagName(t)[0],
+                  k.async=1;k.src=r;a.parentNode.insertBefore(k,a)})
+                  (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+                  ym(103227890, "init", {
+                      clickmap:true,
+                      trackLinks:true,
+                      accurateTrackBounce:true,
+                      webvisor:true
+                  });
+            `}
+          </Script>
+        )}
+
         <meta name="theme-color" content="#0f172a" />
       </head>
       <body>
-        {/* Google Tag Manager noscript (JS o‘chirilgan foydalanuvchilar uchun) */}
+        {/* GTM noscript */}
         {process.env.NODE_ENV === "production" && (
           <noscript>
             <iframe
@@ -54,6 +78,19 @@ export default function RootLayout({
               width="0"
               style={{ display: 'none', visibility: 'hidden' }}
             ></iframe>
+          </noscript>
+        )}
+
+        {/* Yandex Metrika noscript */}
+        {process.env.NODE_ENV === "production" && (
+          <noscript>
+            <div>
+              <img
+                src="https://mc.yandex.ru/watch/103227890"
+                style={{ position: 'absolute', left: '-9999px' }}
+                alt=""
+              />
+            </div>
           </noscript>
         )}
 
