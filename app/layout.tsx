582e81d09166ec2ebd4845a14510f9121fb70433
Khurmatbek <your_email@example.com>
2025-07-07 14:50:40 +0500

    add metrika

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,15 @@
 import type { Metadata } from 'next'
 import './globals.css'
-import Head from 'next/head'
-
-
+import Script from 'next/script' // GTM uchun kerak
 
 export const metadata: Metadata = {
   title: 'Anor International School',
-  description: 'Anor International School - Kelajak uchun ta\'lim berish',
+  description: "Anor International School - Kelajak uchun ta'lim berish",
   themeColor: "#190b2e",
   keywords: [
     'Anor International School',
-    'O\'zbekiston maktabi',
-    'xalqaro ta\'lim',
+    "O'zbekiston maktabi",
+    "xalqaro ta'lim",
     'zamonaviy ta\'lim',
     'o\'quvchilar',
     'o\'qituvchilar',
@@ -30,10 +28,35 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
+      <head>
+        {/* Google Tag Manager script (faqat productionda) */}
+        {process.env.NODE_ENV === "production" && (
+          <Script id="gtm-script" strategy="afterInteractive">
+            {`
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-M99NLGL2');
+            `}
+          </Script>
+        )}
+
         <meta name="theme-color" content="#0f172a" />
-      </Head>
+      </head>
       <body>
+        {/* Google Tag Manager noscript (JS o‘chirilgan foydalanuvchilar uchun) */}
+        {process.env.NODE_ENV === "production" && (
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-M99NLGL2"
+              height="0"
+              width="0"
+              style={{ display: 'none', visibility: 'hidden' }}
+            ></iframe>
+          </noscript>
+        )}
+
         {children}
       </body>
     </html>
